dc2a311e049c1599d9418b2d4b293cf80f2702ce
dipu-37 <razibul1105nstu@gmail.com>
2024-06-23 00:16:44 +0700

    card setup

--- src/component/FeaturedJobs/FeaturedJobs.jsx
+++ src/component/FeaturedJobs/FeaturedJobs.jsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
-import Job from "../Job";
+import Job from "../Job/Job";
 
 
 const FeaturedJobs = () => {
-    const [jobs, setJobs]=useState();
+    const [jobs, setJobs] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('jobs.json')
-        .then(res =>res.json())
-        .then(data =>setJobs(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setJobs(data));
+    }, [])
 
 
     return (
         <div>
-           <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
-           <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-           {
-            jobs.map(job => <Job key={Job.id} job={job}> </Job> )
-           }
+            <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className="grid grid-cols-2 gap-6">
+                {
+                    jobs.map(job => <Job key={Job.id} job={job}> </Job>)
+                }
+            </div>
+
         </div>
     );
 };
